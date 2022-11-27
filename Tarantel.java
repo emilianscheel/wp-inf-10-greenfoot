7e3dd4b3459c6aad1d23f1b976dbaa43eb2dabc7
emilianscheel <emilian-scheel@t-online.de>
2022-11-27 13:07:21 +0100

    adds fliegenFressen(int modus)

--- Tarantel.java
+++ Tarantel.java
@@ -14,40 +14,47 @@ public class Tarantel extends Spinne {
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
 
-    public int kontostand = 0;
+    public int modus = 1;
 
     public void act() {
-        this.fliegenEliminieren();
+        this.fliegenFressen(modus);
     }
 
-    public void fliegenEliminieren() {
-    
-        // Fliegen aufspüren
-        World world = getWorld();
-        List<Fliege> fliegen = world.getObjects(Fliege.class);
+    public void fliegenFressen(int modus) {
+        while (!hindernisVorn()) {
+            
+            this.schritt();
+            
+            if (!this.fliegeAmPlatz()) {
+                continue;
+            }
+            
+            switch (modus) {
+                case 1: 
+                    if (true) {
+                        this.fliegeFressen();
+                    }
+                
+                case 2: 
+                    if (!hindernisLinks() && !hindernisRechts()) {
+                        this.fliegeFressen();
+                    }
 
-        // Sich jede Fliege einzelnt vornehmnen.
-        for (int i = 0; i < fliegen.size(); i++) {
-
-            // Zur Fliege hinteleportieren.
-            setLocation(fliegen.get(i).getX(), fliegen.get(i).getY());
-
-            // Fliege eliminieren.
-            fliegeFressen();
-
-            // Kontostand erhöhen.
-            kontostand ++;
-
-            // Info-Meldung für den Zuschauer geben.
-            JOptionPane.showMessageDialog(null, "-1 addiert auf Anzahl Fliegen", "log",
-                    JOptionPane.ERROR_MESSAGE);
-
-            // Warten, bis die nächste Fliege gefressen wird.
-            warten(1000);
+                case 3: 
+                    if ((hindernisLinks() && !hindernisRechts()) || (!hindernisLinks() && hindernisRechts()) || (!hindernisLinks() && !hindernisRechts())) {
+                        this.fliegeFressen();
+                    }
+                    
+                case 4: 
+                    if (hindernisLinks() && hindernisRechts()) {
+                        this.fliegeFressen();
+                    }
+                
+                case 5: 
+                    if ((hindernisLinks() && hindernisRechts()) || (!hindernisLinks() && !hindernisRechts())) {
+                        this.fliegeFressen();
+                    }
+            }
         }
-
-        JOptionPane.showMessageDialog(null, "Mein Kontostand liegt bei " + kontostand + "; Währung: tote Fliegen",
-                "log",
-                JOptionPane.ERROR_MESSAGE);
     }
 }
